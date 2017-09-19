f0ace76a1a00d7a6db708f3d0d9ee8d3ff399f83
killinsun <noneck.0311@gmail.com>
2017-09-19 06:47:11 -0400

    put testscript

---
+++ test-script.rb
@@ -0,0 +1,22 @@
+require 'net/http'
+require 'uri'
+require 'json'
+require 'openssl'
+
+
+webhookUrl = "https://hooks.tocaro.im/integrations/inbound_webhook/qfxaspzlfbddmg8p6flyqyabr5wcyvhq"
+
+uri = URI.parse(webhookUrl)
+
+params = { text: "test", color: "info", attachments:[{title: "Tocaro integration API test.",value: "This message was send by take-lindev."},{}] }
+
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true
+
+http.set_debug_output $stderr
+
+http.start do |h|
+	request = Net::HTTP::Post.new(uri.path)
+	request.set_form_data(payload: params.to_json)
+	response = h.request(request)
+	end
